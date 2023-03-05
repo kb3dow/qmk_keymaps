89215fdfa792677eaade86566b8b2f1728c3a2a3
Rajarajan Rajamani <r.rajamani@gmail.com>
2023-03-05 10:07:49 -0500

    made dz60 compile with new qmk version

--- keyboards/dz60/keymaps/rrajamani/keymap.c
+++ keyboards/dz60/keymaps/rrajamani/keymap.c
@@ -4,8 +4,6 @@
 // https://www.reddit.com/r/olkb/comments/8cgr26/is_there_a_better_way_taphold_q/
 //RR: Then modified with stuff from -
 //    https://www.keebtalk.com/t/show-me-your-favorite-qmk-hacks/2682/48?u=kb3dow
-// Key must be released within this many milliseconds to be considered a tap
-#define TAPPING_TERM 150
 
 // Timers for tap detection in process_record_user
 uint16_t lalt_timer;
@@ -13,15 +11,16 @@ uint16_t ralt_timer;
 
 // create some custom keycodes for your keymap
 enum custom_keycodes {
-  PREV_DESKTOP = SAFE_RANGE,
+  L_RESET = SAFE_RANGE,
+  PREV_DESKTOP,
   NEXT_DESKTOP
 };
 
 // define some useful send strings
-#define SS_PREV_DESKTOP SS_LCTRL(SS_LGUI(SS_TAP(X_LEFT)))
-#define SS_NEXT_DESKTOP SS_LCTRL(SS_LGUI(SS_TAP(X_RIGHT)))
+//TBD #define SS_PREV_DESKTOP SS_LCTRL(SS_LGUI(SS_TAP(X_LEFT)))
+//TBD #define SS_NEXT_DESKTOP SS_LCTRL(SS_LGUI(SS_TAP(X_RIGHT)))
 
-#define ESC_CTL   LCTL_T(KC_ESC)
+#define CTL_ESC LCTL_T(KC_ESC)
 #define ALT_LDESK (PREV_DESKTOP)
 #define ALT_RDESK (NEXT_DESKTOP)
 #define MY_LYR0 (0)
@@ -35,16 +34,16 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MY_LYR0] = LAYOUT(
-        KC_ESC, KC_1,    KC_2,      KC_3,   KC_4,    KC_5,    KC_6,      KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
+        KC_GRAVE,    KC_1,    KC_2,      KC_3,   KC_4,    KC_5,    KC_6,      KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC, 
         KC_TAB,      KC_Q,    KC_W,      KC_E,   KC_R,    KC_T,    KC_Y,      KC_U,    KC_I,      KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        ESC_CTL,     KC_A,    KC_S,      KC_D,   KC_F,    KC_G,    KC_H,      KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        CTL_ESC,     KC_A,    KC_S,      KC_D,   KC_F,    KC_G,    KC_H,      KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,     XXXXXXX, KC_Z,      KC_X,   KC_C,    KC_V,    KC_B,      KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(MY_LYR2),
         MO(MY_LYR1), KC_LGUI, ALT_LDESK, XXXXXXX, LT(MY_LYR1,KC_SPC), XXXXXXX, ALT_RDESK, KC_RGUI, XXXXXXX, KC_RCTL, MO(MY_LYR1)),
 
     [MY_LYR1] = LAYOUT(
         TO(DEFLYR), KC_F1, KC_F2, KC_F3  ,  KC_F4  ,  KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_INS , KC_DEL,
         _______, _______, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2,  _______, KC_END , KC_PGDN, KC_PGUP, KC_HOME, KC_MPLY, KC_MRWD, KC_MFFD, KC_DEL ,
-        _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,  _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_SCROLLLOCK, KC_PSCREEN, _______,
+        _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,  _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_SCROLL_LOCK, KC_PSCR, _______,
         KC_CAPS, _______, _______, _______,  _______,  _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, TO(MY_LYR2)  , _______, _______,
         _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______, _______),
                                                                 
@@ -96,8 +95,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MS_ACCEL2   ->   KC_ACL2  ->   Set mouse acceleration to 2
 */
     [MY_LYR2] = LAYOUT(                                                     
-        TO(DEFLYR), M(1), M(2), M(3), M(4), M(5), M(6), M(7), M(8), M(9), M(10), M(11), M(12),   KC_BRK, KC_SCROLLLOCK,
-        _______, RGB_M_P, _______, _______,  _______,  _______, _______, _______, _______, _______, BL_TOGG, BL_DEC , BL_INC , RESET,
+        TO(DEFLYR), TO(1), TO(2), TO(3), TO(4), TO(5), TO(6), TO(7), TO(8), TO(9), TO(10), TO(11), TO(12),   KC_BRK, KC_SCROLL_LOCK,
+        _______, RGB_M_P, _______, _______,  _______,  _______, _______, _______, _______, _______, BL_TOGG, BL_DEC , BL_INC , QK_BOOTLOADER,
         _______, RGB_M_B, _______, _______,  _______,  _______, RGB_HUD, RGB_SAD, RGB_SAI, RGB_HUI, _______, _______, _______,
         _______, _______, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_TOG, RGB_VAD, RGB_VAI, RGB_MOD, _______, _______,
         _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______, _______),
@@ -111,7 +110,30 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
+//    LEADER_DICTIONARY() {
+//        leading      = false;
+//        leader_found = false;
+//        SEQ_ONE_KEY(L_RESET) {
+//            leader_found = true;
+//            reset_keyboard();
+//        }
+//        else
+//            SEQ_ONE_KEY(KC_DEL) {
+//            leader_found = true;
+//            layer_clear();
+//        }
+//        else
+//            SEQ_ONE_KEY(LOWER) {
+//            leader_found = true;
+//            layer_on(_LOWER);
+//        }
+//        else
+//            SEQ_ONE_KEY(RAISE) {
+//            leader_found = true;
+//            layer_on(_RAISE);
+//        }
+//        leader_end();
+//    }
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -126,9 +148,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Deactivate LALT
         SEND_STRING(SS_UP(X_LALT));
         // If the action was a tap
-        if (timer_elapsed(lalt_timer) < TAPPING_TERM) {
-          SEND_STRING(SS_PREV_DESKTOP);
-        }
+        //TBD if (timer_elapsed(lalt_timer) < TAPPING_TERM) {
+          //TBD SEND_STRING(SS_PREV_DESKTOP);
+        //TBD }
       }
       return false;
       break;
@@ -142,9 +164,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Deactivate RALT
         SEND_STRING(SS_UP(X_RALT));
         // If the action was a tap
-        if (timer_elapsed(ralt_timer) < TAPPING_TERM) {
-          SEND_STRING(SS_NEXT_DESKTOP);
-        }
+        //TBD if (timer_elapsed(ralt_timer) < TAPPING_TERM) {
+          //TBD SEND_STRING(SS_NEXT_DESKTOP);
+        //TBD }
       }
       return false;
       break;
