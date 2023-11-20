fd673fb0043c0ea0f46192a755c002a12837e0f8
Rajarajan Rajamani <r.rajamani@gmail.com>
2023-11-19 21:06:15 -0500

    fixes for raise/lower

--- keyboards/planck/keymaps/rrajamani/keymap.c
+++ keyboards/planck/keymaps/rrajamani/keymap.c
@@ -16,6 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
+
 enum planck_layers {
     _BASE,
     _LOWER,
@@ -26,6 +27,7 @@ enum planck_layers {
 
 enum planck_keycodes {
     L_RESET = SAFE_RANGE,   // L_RESET NOT USED for now
+    BSPCDEL,
     SHFT_CAPS
 };
 
@@ -46,6 +48,7 @@ float planck_sound[][2] = SONG(PLANCK_SOUND);
 #define FNLYR MO(_FNLYR)    // FUNCTION LAYER
 
 #define SFTENT KC_SFTENT
+#define KC_BSDEL BSPCDEL
 #define CTL_ESC LCTL_T(KC_ESC)
 #define SPC_LWR LT(_LOWER, KC_SPC)
 #define SPC_RAS LT(_RAISE, KC_SPC)
@@ -53,29 +56,33 @@ float planck_sound[][2] = SONG(PLANCK_SOUND);
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base
      * ,-----------------------------------------------------------------------------------.
-     * |  Tab |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | \|   |
+     * |  Tab |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |<-/-> |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |EscCTL| A/Gui| S/Alt| D/Shf|F/Ctrl|   G  |   H  |J/Ctrl|K/Shft|L/Alt |;/Gui |  Ent |
+     * |EscCTL|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ;/: | '\"  |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Fn   | GUI  |  Alt |  TBD | Lower/Space | Raise/Space |  TBD |  Alt | GUI  |  Fn  |
+     * | Fn   | GUI  |  Alt |  -/_ | Lower/Space | Raise/Space |  =/+ |  Alt | GUI  |  Fn  |
      * `-----------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_planck_grid(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-	// Changes for home row mods
-        // CTL_ESC, LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-	CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSDEL,
+	CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 	FNLYR,   KC_LGUI, KC_LALT, KC_MINS, _______, SPC_LWR, _______, SPC_RAS, KC_EQL,  KC_RALT, KC_RGUI, FNLYR /*KC_LEAD,*/
     ),
 
+    [_FNLYR] = LAYOUT_planck_grid(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______
+    ),
     /* Lower
      * ,-----------------------------------------------------------------------------------.
      * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  Bsp |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  '   |
+     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |   +  |   {  |   }  |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -83,9 +90,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_LOWER] = LAYOUT_planck_grid(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -101,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_RAISE] = LAYOUT_planck_grid(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_DQUO,
-        KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -236,4 +243,33 @@ bool music_mask_user(uint16_t keycode) {
         default:
             return true;
     }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static bool bsdel_mods = false;
+  switch(keycode) {
+    //Do a DEL if shift BSP
+    //Ref: https://www.reddit.com/r/olkb/comments/byds0h/shift_space_and_shift_backspace/
+    case BSPCDEL:
+      if (record->event.pressed) {
+        if (get_mods() & MOD_BIT(KC_LSFT)) {
+          unregister_code(KC_LSFT);
+          register_code(KC_DEL);
+          bsdel_mods = true;
+        } else {
+          register_code(KC_BSPC);
+        }
+      } else {
+        if (bsdel_mods) {
+          unregister_code(KC_DEL);
+          register_code(KC_LSFT);
+          bsdel_mods = false;
+        } else {
+          unregister_code(KC_BSPC);
+        }
+      }
+      return false;
+      break;
+  }
+  return true;
 }
